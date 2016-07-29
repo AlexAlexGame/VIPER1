266c75f3bcef419fcb6a55041fcd3f5fce5cad14
AlexAlexGame <listru2005@mail.ru>
2016-07-29 22:06:54 +0300

    Initial Commit

---
+++ VIPER1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VIPER1
+//
+//  Created by alex zolin on 29.07.16.
+//  Copyright © 2016 alex zolin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
